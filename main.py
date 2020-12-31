68d162a5710d6fbfb63e8062b8751c27cc295f76
Алексей Куликов <alex@MacBook-Pro-Aleksej.local>
2020-12-31 11:14:54 +0300

    commit_003_31.12.2020

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 """Импортируем библиотеки"""
 from termcolor import colored
 import time
+import datetime
 
 """Импортируем файлы"""
 #import modbus_Slave_001
 #import modbus_Slave_002
 import MYsql
-import send_to_site
+#import send_to_site
 import static_var
+
 
 
 """функция вызова результатов в терминал"""
 def print_Data_001(i):
+
+    """Запрос точного времени"""
+    time_now = datetime.datetime.now()
+
+    """вывод заголовка в терминал"""
+    print(colored("Result №  " + str(i) + ":", 'red', attrs=['reverse', 'blink']))
+
+    """функции опроса слэйв устройств"""
     static_var.loadA()
     static_var.SS1.printer()
     static_var.SS2.printer()
     static_var.SS3.printer()
     print(' ')
+
+    """функции опроса слэйв устройств"""
     #import modbus_Slave_001.loadA()
     #import modbus_Slave_002.loadA()
     #modbus_Slave_001.S1_t1.printer()
@@ -25,14 +37,19 @@ def print_Data_001(i):
     #modbus_Slave_001.S1_h2.printer()
     #modbus_Slave_001.S1_h2.printer()
     #print(' ')
+
+    """функции опроса слэйв устройств"""
     #modbus_Slave_002.S2_t1.printer()
     #modbus_Slave_002.S2_t2.printer()
     #modbus_Slave_002.S2_h1.printer()
     #modbus_Slave_002.S2_h2.printer()
     #modbus_Slave_002.S2_h2.printer()
     #print(' ')
-    print(colored("Result №  " + str(i) + ":", 'red', attrs=['reverse', 'blink']))
-    time.sleep(0.3)
+
+    """запись в базу по условию 0 секунд точного времени"""
+    if time_now.second == 0:
+        MYsql.send_to_database()
+    time.sleep(1)
 
 #i = 0
 #while i > 0:
@@ -40,7 +57,7 @@ def print_Data_001(i):
     #print_Data_001(i)
 
 i = 0
-for i in range(10):
+for i in range(100):
     i = i + 1
     print_Data_001(i)
 
